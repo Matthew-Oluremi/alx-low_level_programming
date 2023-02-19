dd948d7628cf240d7de88b474c83c4dd567bac97
Matthew Oluremi <matthewoluremi376@gmail.com>
2023-02-19 08:00:45 -0800

    Alphabelt

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the aphabet in lower then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m,n;
+
+	m = 97;
+	n = 65;
+
+	while (m < 123)
+	{
+		putchar(m);
+		m++;
+	}
+	
+
+	while (n < 91)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
