ff57a2c48bd08906cd84708fd64046cc50c264e9
Matthew Oluremi <matthewoluremi376@gmail.com>
2023-02-21 10:03:32 -0800

    Print alphabelt

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print alphabelt
+ *
+ * Description: print alphabelt of letters
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int m;
+
+	for (m = 'a'; m <= 'z'; m++)
+
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+}
