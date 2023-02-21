561b622fe96e1e70c289494eaa41024dc3812f93
Matthew Oluremi <matthewoluremi376@gmail.com>
2023-02-21 07:30:39 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar
+ *
+ * Description: print _putchar
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
