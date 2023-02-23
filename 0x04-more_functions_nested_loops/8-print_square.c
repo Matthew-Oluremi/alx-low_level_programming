17a5f10a2c84d722bce441966df6e4f26d6dd4be
Matthew Oluremi <matthewoluremi376@gmail.com>
2023-02-23 07:16:37 -0800

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print a square, followed by a new line;
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int m, n;
+
+		for (m = 0; m < size; m++)
+		{
+			for (n = 0; n < size; n++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
