f56b577a9fa4230e6171d9dfa1a557bae1764df9
Matthew Oluremi <matthewoluremi376@gmail.com>
2023-02-23 05:43:12 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * followed by a new line
+ */
+
+void print_numbers(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+		_putchar(m + '0');
+	}
+
+	_putchar('\n');
+}
+
