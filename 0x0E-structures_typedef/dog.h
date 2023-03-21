5f7e8845d6a2609e4b04ef9d6cd15382c62854b6
Matthew Oluremi <matthewoluremi376@gmail.com>
2023-03-20 20:21:50 -0700

    new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,16 @@
 #ifndef _DOG_
 #define _DOG_
 
-
 /**
- * dog_t - Defines a new type dog_t as a new name for the type struct dog.
- *
+ * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
-
 /**
- * struct dog - Defines a new type struct dog.
+ * struct dog - Struct defines a new type struct dog.
  *
- * Description: User defined that type called "dog", stores name,
- * age and owner.
+ * Description: Struct is a user defined that type called "dog",
+ * stores name, age and owner.
  *
  * @name: Dog's name.
  * @age: Dog's age.
@@ -27,7 +23,6 @@ struct dog
 	float age;
 	char *owner;
 };
-
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
