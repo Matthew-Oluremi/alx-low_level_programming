f71a76059d3c24d62ee7435aeaff21e391e29c2d
Matthew Oluremi <matthewoluremi376@gmail.com>
2023-03-20 17:11:08 -0700

    A program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Main program that prints the name of the file
+ * it was compiled from, followed by a new line.
+ *
+ * Retun: Returns 0 (Success).
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
